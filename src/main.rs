427b328628443217463a75920ce78e70f88276a1
unegare <dronte2008@yandex.ru>
2019-07-24 20:45:13 +0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,13 @@ use rand::Rng;
 
 use serde::{Deserialize, Serialize};
 
-use std::io::{BufWriter, Write/*, Read*/};
+use std::io::{/*BufWriter,*/ Write/*, Read*/};
 
 //use tokio_io::AsyncWrite;
 
 use failure::Fail;
 
-use image::{ImageFormat, guess_format};
+use image::{ImageFormat/*, guess_format*/};
 
 use futures::stream::Stream;
 
@@ -89,6 +89,80 @@ impl FilenameGenerator for Gen {
     }
 }
 
+#[derive(Debug, Fail)]
+enum PIError {
+    #[fail(display = "Format Guessing Error")]
+    FormatGuessing(String),
+//    #[fail(display = "Unsupported Format Error, {}", _0)]
+    #[fail(display = "Unsupported Format Error")]
+    UnsupportedFormat(String),
+    #[fail(display = "Internal Server IO Error")]
+    IO(String),
+    #[fail(display = "Decoding Image Error")]
+    Loading(String)
+}
+
+fn process_image (data: &[u8]) -> Result<String,PIError/*std::io::Error*/> {
+    let fmt = match image::guess_format(&data) {
+        Ok(fmt) => fmt,
+        Err(e) => {
+            eprintln!("process_image: {:?}", e);
+            return Err(PIError::FormatGuessing(format!("{:?}", e)));
+        }
+    };
+    let ext = match fmt {
+        ImageFormat::PNG => "png",
+        ImageFormat::JPEG => "jpg",
+        ImageFormat::GIF => "gif",
+//                                ImageFormat::WEBP => "webp",
+        ImageFormat::PNM => "pnm",
+//                                ImageFormat::TIFF => "tiff",
+        ImageFormat::TGA => "tga",
+        ImageFormat::BMP => "bmp",
+        ImageFormat::ICO => "ico",
+        ImageFormat::HDR => "hdr",
+
+        _ => {
+            eprintln!("process_image: match fmt: unsupported format");
+            return Err(PIError::UnsupportedFormat(format!("{:?}", fmt)));
+        }
+    };
+    let mut pb = get_random_name();
+    match image::load_from_memory(&data) {
+        Ok(img) => {
+            let mut pbthumb = pb.clone();
+            pbthumb.set_extension(String::from("thumb.") + &ext);
+            match img.thumbnail_exact(100,100).save(&pbthumb) {
+                Ok(_) => {},
+                Err(e) => {
+                    eprintln!("process_image: thumbnail_exact.save : {:?}", e);
+                    return Err(PIError::IO(format!("{:?}", e)));
+                }
+            };
+            pb.set_extension(ext);
+            let mut fimg = match std::fs::File::create(&pb) {
+                Ok(f) => f,
+                Err(e) => {
+                    eprintln!("process_image: fimg create : {:?}", e);
+                    return Err(PIError::IO(format!("{:?}", e)));
+                }
+            };
+            match fimg.write_all(&data) {
+                Ok(_) => {},
+                Err(e) => {
+                    eprintln!("process_image: fimg.write_all : {:?}", e);
+                    return Err(PIError::IO(format!("{:?}", e)));
+                }
+            };
+        },
+        Err(e) => {
+            eprintln!("process_image: image::load_from_memory : {:?}", e);
+            return Err(PIError::Loading(format!("{:?}", e)));
+        }
+    }
+    Ok(pb.to_str().unwrap().to_string())
+}
+
 #[derive(Serialize)]
 struct ResKeys {
     keys: Vec<String>
@@ -98,13 +172,12 @@ fn upload_multipart((mp, _state): (Multipart, Data<Form>)) -> Box<dyn Future<Ite
 //    println!("state: {:?}", state);
     Box::new( 
         mp
-            .map_err(error::ErrorInternalServerError)
-            .map(move |field| {
+            .map_err(error::ErrorBadRequest)
+            .map(move |field: actix_multipart::Field| {
                 if false {return Either::A(err(error::ErrorInternalServerError(""))).into_stream()}
                 Either::B(field.fold(Vec::new(), |mut acc: Vec<u8>, b: bytes::Bytes| {
-//                    println!("after fold ... bytes.len() == {} | da.len() == {}", b.len(), acc.len());
                     acc.append(&mut b.to_vec());
-                    /*actix_web::web::block*/futures::lazy(move || {
+                    futures::lazy(move || {
                         Ok(acc)
                     })
                         .map_err(|_e: error::BlockingError<std::io::Error>| {
@@ -112,100 +185,39 @@ fn upload_multipart((mp, _state): (Multipart, Data<Form>)) -> Box<dyn Future<Ite
                         })
                 })
                     .map(|v| {
-//                        println!("v.len() == {}", v.len());
-                        actix_web::web::block(move || {
-                            let fmt = match image::guess_format(&v) {
-                                Ok(fmt) => fmt,
-                                Err(e) => {
-                                    eprintln!("{:?}", e);
-                                    return Err(MultipartError::Incomplete);
-                                }
-                            };
-                            let ext = match fmt {
-                                ImageFormat::PNG => "png",
-                                ImageFormat::JPEG => "jpg",
-                                ImageFormat::GIF => "gif",
-//                                ImageFormat::WEBP => "webp",
-                                ImageFormat::PNM => "pnm",
-//                                ImageFormat::TIFF => "tiff",
-                                ImageFormat::TGA => "tga",
-                                ImageFormat::BMP => "bmp",
-                                ImageFormat::ICO => "ico",
-                                ImageFormat::HDR => "hdr",
-
-                                _ => {
-                                    eprintln!("match fmt: unsupported format");
-                                    return Err(MultipartError::Incomplete);
-                                }
-                            };
-                            let mut pb = get_random_name();
-                            match image::load_from_memory(&v) {
-                                Ok(img) => {
-                                    let mut pbthumb = pb.clone();
-                                    pbthumb.set_extension(String::from("thumb.") + &ext);
-                                    match img.thumbnail_exact(100,100).save(&pbthumb) {
-                                        Ok(_) => {},
-                                        Err(e) => {
-                                            eprintln!("upload_multipart: thumbnail_exact.save : {:?}", e);
-                                            return Err(MultipartError::Incomplete);
+                        actix_web::web::block(move || process_image(&v))
+                            .map_err(|e: error::BlockingError<PIError>| {
+                                match e {
+                                    error::BlockingError::Error(e) => {
+                                        match e {
+                                            PIError::FormatGuessing(_) => error::ErrorBadRequest(e),
+                                            PIError::UnsupportedFormat(_) => error::ErrorBadRequest(e),
+                                            PIError::Loading(_) => error::ErrorBadRequest(e),
+                                            PIError::IO(_) => error::ErrorInternalServerError(e),
                                         }
-                                    };
-                                    pb.set_extension(ext);
-                                    let mut fimg = match std::fs::File::create(&pb) {
-                                        Ok(f) => f,
-                                        Err(e) => {
-                                            eprintln!("upload_multipart: fimg create : {:?}", e);
-                                            return Err(MultipartError::Incomplete);
-                                        }
-                                    };
-//                                    let bufw = std::io::BufWriter::new(fimg);
-                                    match fimg.write_all(&v) {
-                                        Ok(_) => {},
-                                        Err(e) => {
-                                            eprintln!("upload_multipart: fimg.write_all : {:?}", e);
-                                            return Err(MultipartError::Incomplete);
-                                        }
-                                    };
-//                                    match img.save(&pb) {
-//                                        Ok(_) => {},
-//                                        Err(e) => {
-//                                            eprintln!("upload_multipart: img.save : {:?}", e);
-//                                            return Err(MultipartError::Incomplete);
-//                                        }
-//                                    };
-                                },
-                                Err(e) => {
-                                    eprintln!("upload_multipart: image::load_from_memory : {:?}", e);
-                                    return Err(MultipartError::Incomplete);
+                                    }
+                                    error::BlockingError::Canceled => error::ErrorInternalServerError("")
                                 }
-                            }
-                            Ok(pb.to_str().unwrap().to_string())
-                        })
-                            .map(|s| s)
-                            .map_err(|_e: error::BlockingError<MultipartError>| {MultipartError::Incomplete})
+                            })
                     })
-                    .map_err(|e| error::ErrorInternalServerError(e))
+                    .map_err(|e: MultipartError| {
+                        error::ErrorBadRequest(e)
+                    })
                 )
                     .into_stream()
             })
             .flatten()
             .collect()
-            .map_err(|e| {
-                eprintln!("failed1: {:?}", e);
-                MultipartError::Incomplete
-            })
-            .and_then(|v| futures::future::join_all(v))
-            .map_err(|e| {
-                eprintln!("failed2: {:?}", e);
-                error::ErrorInternalServerError(e)
+            .and_then(|v| {
+                futures::future::join_all(v)
             })
             .and_then(|v| {
 //                println!("v == {:?}", v);
                 HttpResponse::Created().json(ResKeys{keys: v})
             })
             .map_err(|e| {
-                eprintln!("failed3: {:?}", e);
-                error::ErrorInternalServerError(e)
+                eprintln!("upload_multipart ended up with a error: {:?}", e);
+                e
             })
     )
 }
@@ -219,16 +231,6 @@ struct FileDescriptor {
 #[derive(Deserialize, Debug)]
 struct MyJson {
     arr: Vec<FileDescriptor>
-}
-
-#[derive(Debug, Fail)]
-enum JsonError {
-    #[fail(display = "format guessing error")]
-    FormatGuessingError,
-    #[fail(display = "unsupported format error")]
-    UnsupportedImageFormat,
-    #[fail(display = "internal server error")]
-    InternalServerError
 }
 
 fn upload_json(json: actix_web::web::Json<MyJson>) -> Box<dyn Future<Item = HttpResponse, Error = actix_http::error::Error>> {
@@ -247,77 +249,9 @@ fn upload_json(json: actix_web::web::Json<MyJson>) -> Box<dyn Future<Item = Http
     Box::new(
         actix_web::web::block(move || {
             let mut reskeys = ResKeys{keys: Vec::with_capacity(files_data.len())};
-//            let mut vfut = Vec::new();
             for fd in files_data {
-                let fmt = match guess_format(&fd) {
-                    Ok(fmt) => fmt,
-                    Err(e) => {
-                        eprintln!("upload_json: guess_format : {:?}", e);
-                        return Err(JsonError::FormatGuessingError)
-                    }
-                };
-                let mut pb = get_random_name();
-                match fmt {
-                    ImageFormat::PNG => {
-                        pb.set_extension("png")
-                    },
-                    ImageFormat::JPEG => pb.set_extension("jpg"),
-                    ImageFormat::GIF => pb.set_extension("gif"),
-//                    ImageFormat::WEBP => pb.set_extension("webp"),
-                    ImageFormat::PNM => pb.set_extension("pnm"),
-//                    ImageFormat::TIFF => pb.set_extension("tiff"),
-                    ImageFormat::TGA => pb.set_extension("tga"),
-                    ImageFormat::BMP => pb.set_extension("bmp"),
-                    ImageFormat::ICO => pb.set_extension("ico"),
-                    ImageFormat::HDR => pb.set_extension("hdr"),
-
-                    _ => {
-                        eprintln!("upload_json: unsupported fmt");
-                        return Err(JsonError::UnsupportedImageFormat)
-                    }
-                };
-                let mut f = match std::fs::File::create(&pb) {
-                    Ok(handler) => BufWriter::new(handler),
-                    _ => {
-                        eprintln!("upload_json: couldn't create the file {:?}", &pb);
-                        return Err(JsonError::InternalServerError);
-                    }
-                };
-
-                match image::load_from_memory(&fd) {
-                    Ok(img) => {
-                        let mut pb_thumb = pb.clone();
-                        pb_thumb.set_extension("thumb.png");
-                        match img.thumbnail_exact(100,100).save(pb_thumb) {
-                            Ok(_) => {},
-                            Err(e) => {
-                                eprintln!("upload_json: img.thumbnail_exact.save error: {:?}", e);
-                                return Err(JsonError::InternalServerError);
-                            }
-                        };
-                    },
-                    Err(e) => {
-                        eprintln!("upload_json: load_from_memory : {:?}", e);
-                        return Err(JsonError::InternalServerError);
-                    }
-                };
-
-//                if let Ok(handler) = std::fs::File::create(&pb) {
-//                    f = BufWriter::new(handler);
-//                } else {
-//                    eprintln!("couldn't create the file {:?}", &pb);
-//                    return HttpResponse::InternalServerError().finish();
-//                }
-//                vfut.push(f.write_buf(&fd));
-//                vfut.push(AsyncWrite::write_buf(&mut f, &fd));
-                if let Ok(_) = f.write(&fd) {
-                } else {
-                    eprintln!("upload_json: write error into the file {:?}", &pb);
-                    return Err(JsonError::InternalServerError);
-                }
-                reskeys.keys.push(pb.to_str().unwrap().to_string());
+                reskeys.keys.push(process_image(&fd)?);
             }
-//            futures::future::join_all(vfut).wait();
             Ok(serde_json::to_string(&reskeys).unwrap())
         })
             .map(|s| {
@@ -332,24 +266,18 @@ fn upload_json(json: actix_web::web::Json<MyJson>) -> Box<dyn Future<Item = Http
                 res.headers_mut().insert(http::header::CONTENT_TYPE, http::header::HeaderValue::from_str("application/json").unwrap());
                 res
             })
-            .map_err(|e: error::BlockingError<JsonError>| {
+            .map_err(|e: error::BlockingError<PIError>| {
+                eprintln!("upload_json ended up with a error: {:?}", e);
                 match e {
                     error::BlockingError::Error(e) => {
                         match e {
-                            JsonError::FormatGuessingError => {
-                                error::ErrorBadRequest("format guessing error")
-                            },
-                            JsonError::UnsupportedImageFormat => {
-                                error::ErrorBadRequest("unsupported image format")
-                            },
-                            JsonError::InternalServerError => {
-                                error::ErrorInternalServerError("")
-                            }
+                            PIError::FormatGuessing(_) => error::ErrorBadRequest(e),
+                            PIError::UnsupportedFormat(_) => error::ErrorBadRequest(e),
+                            PIError::Loading(_) => error::ErrorBadRequest(e),
+                            PIError::IO(_) => error::ErrorInternalServerError(e),
                         }
-                    },
-                    error::BlockingError::Canceled => {
-                        error::ErrorBadRequest("")
                     }
+                    error::BlockingError::Canceled => error::ErrorInternalServerError("")
                 }
             })
     )
